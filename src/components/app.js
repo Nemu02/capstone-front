ee5f17000cab011b41d4914575d82b35a304d9ca
Nemu02 <neumv2@icloud.com>
2022-12-13 15:40:53 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import NavigationContainer from './navigation/navigation-container';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <NavigationContainer />
+        <h1>Neum John G Vento CapStone</h1>
+      </div>
+    );
+  }
+}
