c2a599468d9363c87b8526f1f7498cc2703f1455
Nemu02 <neumv2@icloud.com>
2022-12-21 18:27:13 -0600

    adding api data

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,49 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import navLogo from "../../../static/assets/logo/logo22.png"
 
-export default class NavigationContainer extends Component {
-    constructor() {
-        super();
-    }
+const NavigationContainer = (props) => {
 
-    render() {
-        return (
-            <div className="navigation-wrapper">
-                <div className="nav-left">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">
-                            Home
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/gear-list" activeClassName="nav-link-active">
-                            Gear List
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/member-list" activeClassName="nav-link-active">
-                            Member List
-                        </NavLink>
-                    </div>
-    
-                    {false ? <button>Issue/DeIssue</button> : null }
-                    {false ? <button>Add Gear</button> : null }
-                    {false ? <button>Add Member</button> : null }
+    return (
+        <div className="navigation-wrapper">
+            <div className="nav-left">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
                 </div>
 
-                <div className="nav-center">
-                    <h1>R.M.G.I.S</h1>
-                    <h4>Random Millitary Gear Issue System</h4>
+                <div className="nav-link-wrapper">
+                    <NavLink to="/gear-list" activeClassName="nav-link-active">
+                        Gear List
+                    </NavLink>
                 </div>
 
-                <div className="nav-right">
-                    <img src={navLogo} alt='Logo' />
+                <div className="nav-link-wrapper">
+                    <NavLink to="/edit-gear" activeClassName="nav-link-active">
+                        <FontAwesomeIcon icon="pen-to-square" />
+                    </NavLink>
                 </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/member-list" activeClassName="nav-link-active">
+                        Member List
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/edit-member" activeClassName="nav-link-active">
+                        <FontAwesomeIcon icon="pen-to-square" />
+                    </NavLink>
+                </div>
+
+                {false ? <button>Issue/DeIssue</button> : null }
+                {false ? <button>Add Gear</button> : null }
+                {false ? <button>Add Member</button> : null }
             </div>
-        )
-    }
+
+            <div className="nav-center">
+                <h1>R.M.G.I.S</h1>
+                <h4>Random Millitary Gear Issue System</h4>
+            </div>
+
+            <div className="nav-right">
+                <img src={navLogo} alt='Logo' />
+            </div>
+        </div>
+    )
+    
 }
+
+export default NavigationContainer;
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -3,24 +3,43 @@ import Login from '../auth/login';
 import authLoginImage from "../../../static/assets/background/auth.jpg";
 
 export default class Auth extends Component {
-  render() {
-    return (
-      <div className='auth-page-wrapper'>
-        <div className='auth-left-side'>
-            <Login />
+    constructor(props) {
+        super(props);
+
+        this.handleGoodAuth = this.handleGoodAuth.bind(this);
+        this.handleBadAuth = this.handleBadAuth.bind(this);
+    }
+
+    handleGoodAuth() {
+        this.props.handleSuccessfulLogin();
+        this.props.history.push("/");
+    }
+
+    handleBadAuth() {
+        this.props.handleUnSuccessfulLogin();
+    }
+
+    render() {
+        return (
+        <div className='auth-page-wrapper'>
+            <div className='auth-left-side'>
+                <Login 
+                    handleGoodAuth={this.handleGoodAuth}
+                    handleBadAuth={this.handleBadAuth}
+                />
+            </div>
+
+            <div 
+                className='auth-right-side' 
+                style={{
+                    backgroundImage: `url(${authLoginImage})`
+                }}
+            
+            />
+
+            
+
         </div>
-
-        <div 
-            className='auth-right-side' 
-            style={{
-                backgroundImage: `url(${authLoginImage})`
-            }}
-        
-        />
-
-        
-
-      </div>
-    )
-  }
+        )
+    }
 }
